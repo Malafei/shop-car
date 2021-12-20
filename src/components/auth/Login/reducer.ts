1eeefe3dbb5e6286420e8631b1b4976c32e3af74
MaxMalafei <ukrainelove8@gmail.com>
2021-12-20 23:24:34 +0200

    довідправив

--- src/components/auth/Login/reducer.ts
+++ src/components/auth/Login/reducer.ts
@@ -9,15 +9,15 @@ const initialState: AuthState = {
 export const authReducer = (state=initialState, action: AuthAction) : AuthState => {
     switch (action.type) {
 
-      case AuthActionTypes.LOGIN_AUTH:
-        return {
+      case AuthActionTypes.LOGIN_AUTH: //ловим екшн логін
+        return { //міняємо дані
           ...state,
           isAuth: true,
           user: action.payload,
         };
 
-      case AuthActionTypes.LOGOUT_AUTH:
-        return {
+      case AuthActionTypes.LOGOUT_AUTH: //ловим екшн логаут
+        return { //міняємо дані
           ...state,
           isAuth: false,
           user: {
